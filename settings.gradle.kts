b789995dd1f8753ec7be8d160493c4d40f82f630
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-11-14 15:17:06 +0100

    ci: added semver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2024 Fraunhofer IEM. All rights reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ *
+ * SPDX-License-Identifier: MIT
+ * License-Filename: LICENSE
+ */
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
 
 rootProject.name = "spha-cli"
