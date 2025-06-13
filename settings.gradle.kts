72d9e1078b85fabb79ccf2bef4276bad87e93238
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2025-06-13 14:25:10 +0200

    chore: bumped the foojay-resolver version to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Fraunhofer IEM. All rights reserved.
+ * Copyright (c) 2024-2025 Fraunhofer IEM. All rights reserved.
  *
  * Licensed under the MIT license. See LICENSE file in the project root for details.
  *
@@ -9,6 +9,6 @@
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 rootProject.name = "spha-cli"
